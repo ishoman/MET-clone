9c578ba49de250b65f8cb4e1f10daaf4bfae4886
Isaac Shoman <isaac.shoman@gmail.com>
2018-05-04 14:08:23 -0700

    Add events array to event component.ts

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Event } from '../models/events.model';
 
 @Component({
@@ -6,12 +6,18 @@ import { Event } from '../models/events.model';
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
-export class EventsComponent implements OnInit {
 
-  constructor() { }
+export class EventsComponent {
 
-  // Event {
-  //     constructor(public eventName: string, public eventDetails: string, public eventTime: string, eventCategory: string){}
-  // }
+events: Event[] = [
+  new Event('Fellows Colloquium—Creation, Devotion, and Destruction of Objects', 'Bonnie J. Sacerdote Lecture Hall, Uris Center for Education / Free with Museum admission', '10:00 A.M.', 'SYMPOSIUM'),
+  new Event('The Met Store Spring Rug Event', 'The Met Store / Free with Museum admission', '10:00 A.M.', 'MET STORE EVENT'),
+  new Event('Masterpieces of the Middle Ages', 'Tours Sign, Great Hall / Free with Museum admission', '10:45 A.M.', 'GUIDED TOUR'),
+  new Event('博物館精華 (Museum Highlights in Chinese/Mandarin', 'International Tours Sign, Great Hall / Free with Museum admission', '10:45 A.M.', 'GUIDED TOUR')
+]
+  constructor() {
+    console.log(this.events)
+   }
+
 
 }
