a65016d8966b1f884de179a7d9d20af081c03753
Isaac Shoman <isaac.shoman@gmail.com>
2018-05-04 13:08:58 -0700

    Begin constructing museum location array for looping

--- src/app/museum-loc/museum-loc.component.ts
+++ src/app/museum-loc/museum-loc.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Museum } from '../models/museum-location-info.model'
 
 @Component({
   selector: 'app-museum-loc',
   templateUrl: './museum-loc.component.html',
   styleUrls: ['./museum-loc.component.css']
 })
-export class MuseumLocComponent implements OnInit {
+export class MuseumLocComponent {
 
-  constructor() { }
+  museums: Museum[] = [
+    new Museum(locationName: 'The Met Fifth Avenue', description: 'Over 5,000 years of art from every corner of the world' ),
+    new Museum()
+  ]
 
-  ngOnInit() {
-  }
+
+
+
 
 }
